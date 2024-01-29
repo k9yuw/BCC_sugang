7b082997be70961967c011abdd680dcab7f7261b
minwin00 <rst0272@naver.com>
2024-01-29 13:07:55 +0900

    :memo: added sugang Click

---
+++ app/components/src/ClickGame.js
@@ -0,0 +1,55 @@
+import {useState, useEffect} from "react";
+// import SuccessOrFail from "./SuccessOrFail";
+import ResultPopUp from "./ResultPopUp";
+import WaitingPopUp from "./WatingPopUp";
+
+//신청 버튼
+function ClickGame(){
+  // const [clickTime, setClickTime] = useState(0);
+  // const onClick = (event) => {
+  //   // setClickTime(event.target.
+  //   console.log(event);
+  // };
+  const [startTime, setStartTime] = useState(0);
+  const [clickTime, setClickTime] = useState(0);
+  const [startText, setStartText] = useState("타이머 시작");
+  //시작 클릭 시 타이머(시간측정) 시작
+  const startSugang = () => {
+    const time = new Date();
+    setStartTime(time.getTime());
+    setClickTime(0);
+    setStartText("다시 시작");
+  };
+  //신청 클릭 시 시작 시간과의 차(밀리초) 계산
+  const onClick = () => {
+    const tempTime = new Date();
+    setClickTime(tempTime.getTime());
+
+  };
+  const timeTaken = clickTime - startTime
+  console.log(`시작 ${startTime}`);
+  console.log(`클릭 ${clickTime}`);
+  console.log(timeTaken);
+  //useEffect(() =>{
+
+	return (
+		<div>
+      <div>
+        <button onClick={startSugang}>{startText}</button>
+      </div>
+      <div>
+        <button onClick={onClick}>신청</button>
+        {(clickTime !=0 && timeTaken< 1000) ? <ResultPopUp resultType = "toEarly"/> : null}
+        {(clickTime !=0 && timeTaken > 1000) ? <WaitingPopUp timeTaken={timeTaken} rand={Math.random()}/> : null}
+      </div>
+      <div>
+        <h3>클릭 시간: {timeTaken/1000} 초</h3>
+      </div>
+    </div>
+	);
+}
+
+
+
+
+export default ClickGame;
