c11af8faca9e904c0392486a37717bfb91eeaa1b
chelsh <sinye2002@korea.ac.kr>
2024-02-04 14:13:00 +0900

    :bug: debug trivial errors

--- app/components/clock/clock.tsx
+++ app/components/clock/clock.tsx
@@ -2,15 +2,17 @@
 
 import { useState, useEffect, useRef } from "react";
 import styles from "./navysm.module.css";
-import bgm from './bgm.mp3';
+// import bgm from './bgm.mp3';
+import * as bgm from "./bgm.mp3";
 
 const Clock: React.FC = () => {
-  const [date, setDate] = useState<number>(new Date(2024, 1, 13, 9, 59, 50).getTime());
-  const [timeFormat, setTimeFormat] = useState<string>(""); 
-  const [msFormat, setMsFormat] = useState<string>(""); 
+  const [date, setDate] = useState<number>(
+    new Date(2024, 1, 13, 9, 59, 50).getTime()
+  );
+  const [timeFormat, setTimeFormat] = useState<string>("");
+  const [msFormat, setMsFormat] = useState<string>("");
   const [isRed, setIsRed] = useState<boolean>(false);
   const [bgmPlayed, setBgmplayed] = useState<boolean>(false);
-  
 
   const clockRef = useRef(null);
   const msCheckboxRef = useRef(null);
@@ -24,21 +26,23 @@ const Clock: React.FC = () => {
       const hours = String(new Date(date).getHours()).padStart(2, "0");
       const minutes = String(new Date(date).getMinutes()).padStart(2, "0");
       const seconds = String(new Date(date).getSeconds()).padStart(2, "0");
-      const milliSeconds = String(new Date(date).getMilliseconds()).padStart(3,"0");
+      const milliSeconds = String(new Date(date).getMilliseconds()).padStart(
+        3,
+        "0"
+      );
 
       setTimeFormat(`${hours}시 ${minutes}분 ${seconds}초 `);
       setMsFormat(`${milliSeconds}`);
 
-      if (hours === '09' && minutes === '59' && seconds >= '53') {
+      if (hours === "09" && minutes === "59" && seconds >= "53") {
         setIsRed(true);
         setBgmplayed(true);
-      } else if (hours === '10' && minutes === '00' && seconds === '03') {
+      } else if (hours === "10" && minutes === "00" && seconds === "03") {
         setIsRed(false);
         setBgmplayed(false);
       }
-      
     }, 10);
-    
+
     return () => clearInterval(timerId);
   }, [date]); // date가 변경될 때마다 useEffect 실행
 
@@ -49,40 +53,35 @@ const Clock: React.FC = () => {
       bgmRef.current?.pause();
       bgmRef.current.currentTime = 0; // 시작 지점으로 되돌림
     }
-  }, [bgmPlayed]); 
+  }, [bgmPlayed]);
 
-  const backgroundClass = isRed ? styles.redBackground : '';
+  const backgroundClass = isRed ? styles.redBackground : "";
 
-return (
-  <div className={`${backgroundClass}`}>
-    <div className="position">
-      <div className={styles.timeArea}>
-        <div ref={clockRef} id="clock" className={styles.clockWithMsec}>
-          {timeFormat}
-          {msCheckboxRef.current?.checked && (
-            <div className={styles.msecArea}>
-              {msFormat}
-            </div>
-          )}
+  return (
+    <div className={`${backgroundClass}`}>
+      <div className="position">
+        <div className={styles.timeArea}>
+          <div ref={clockRef} id="clock" className={styles.clockWithMsec}>
+            {timeFormat}
+            {msCheckboxRef.current?.checked && (
+              <div className={styles.msecArea}>{msFormat}</div>
+            )}
+          </div>
+        </div>
+
+        <div className={styles.checkboxes}>
+          <label>
+            <input ref={msCheckboxRef} type="checkbox" id="msCheckbox" />
+            밀리초 보기
+          </label>
+          <label>
+            <input ref={bgmCheckboxRef} type="checkbox" id="bgmCheckbox" />
+            음악 듣기
+          </label>
         </div>
       </div>
-      
-      <div className={styles.checkboxes}> 
-        <label>
-          <input ref={msCheckboxRef} type="checkbox" id="msCheckbox" />밀리초 보기   
-        </label>
-        <label>
-          <input ref={bgmCheckboxRef} type="checkbox" id="bgmCheckbox" />음악 듣기
-        </label>
-      </div>
+      <audio ref={bgmRef} src={bgm} id="backgroundMusic" />
     </div>
-    <audio ref={bgmRef} src={bgm} id="backgroundMusic" />
-  </div>
-);
-
-}
-  export default Clock;
-  
-
-  
-
+  );
+};
+export default Clock;
--- app/searchCourses/page.tsx
+++ app/searchCourses/page.tsx
@@ -29,8 +29,47 @@ export default function Home() {
   const [section, setSection] = useState(""); //분반
   const [courseName, setCourseName] = useState(""); //교과목명
   const id = localStorage.getItem("username");
-  let searchedData = undefined;
+  const [searchedData, setSearchedData] = useState<courseData[]>();
   const [searched, setSearched] = useState(false);
+
+  interface courseData {
+    lmt_yn: string;
+    nemo_yn: string;
+    eng100: string;
+    year: string;
+    tutorial_yn: string;
+    mooc_yn: string;
+    isu_nm: string;
+    courgrad_cd: string;
+    cour_cd: string;
+    rowid: number;
+    enable: string;
+    cour_div: string;
+    term: string;
+    credit: number;
+    department: string;
+    flexible_school_yn: string;
+    attend_free_yn: string;
+    flexible_term: number;
+    flipped_class_yn: string;
+    absolute_yn: string;
+    time_room: string;
+    campus: string;
+    flexible_to_dt: string;
+    flexible_fr_dt: string;
+    prof_nm: string;
+    waiting_yn: string;
+    params: string;
+    cour_nm: string;
+    dept_cd: string;
+    cour_cls: string;
+    drop_lmt_yn: string;
+    time: string;
+    apply_dept: string;
+    exch_cor_yn: string;
+    no_supervisor_yn: string;
+    col_cd: string;
+  }
 
   const router = useRouter();
   const onClickPreferredCourses = (e: MouseEvent<HTMLSpanElement>) => {
@@ -57,7 +96,7 @@ export default function Home() {
     e.preventDefault();
     setSearched(true);
     if (selectedIdxOne === 0) {
-      searchedData = major[selectedIdxTwo][selectedIdxThree];
+      setSearchedData(major[selectedIdxTwo][selectedIdxThree]);
       console.log(searchedData);
     }
   };
@@ -1860,14 +1899,14 @@ export default function Home() {
               </thead>
               {searchedData ? (
                 <tbody style={{ fontSize: 12 }}>
-                  {searchedData.map((prop) => (
+                  {searchedData.map((prop: courseData) => (
                     <tr
                       style={{
                         fontSize: 12,
                         textAlign: "center",
                         height: 34,
                       }}
-                      key={prop}
+                      key={prop.params}
                     >
                       <td>{prop.campus}</td>
                       <td>{prop.cour_cd}</td>
