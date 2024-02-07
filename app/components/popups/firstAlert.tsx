42a1d5e2b9f196206a2c493f977bd01df63a41d1
chelsh <sinye2002@korea.ac.kr>
2024-02-07 15:17:19 +0900

    update

--- app/components/popups/firstAlert.tsx
+++ app/components/popups/firstAlert.tsx
@@ -1,78 +1,128 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 interface ModalProps {
   isOpen: boolean;
   closeModal: () => void;
 }
 
 const FirstAlert: React.FC<ModalProps> = ({ isOpen, closeModal }) => {
-  const customStyles={
-		overlay: {
-			backgroundColor: "rgba(0,0,0,0.7)",
-		},
-		content:{
-			backgroundColor: "white",
-			width:"500px",	
-			height: "230px",
-			margin: "auto",
-			borderRadius: "8px",
-			borderColor: "rgba(0,0,0,0.1)",
-			boxShadow: "0, 2px 4px rgba(0,0,0,0.1)",
-			padding: "8px",
-		},
-	};
+  const customStyles = {
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.7)",
+    },
+    content: {
+      backgroundColor: "white",
+      width: "500px",
+      height: "230px",
+      margin: "auto",
+      borderRadius: "8px",
+      borderColor: "rgba(0,0,0,0.1)",
+      boxShadow: "0, 2px 4px rgba(0,0,0,0.1)",
+      padding: "8px",
+    },
+  };
 
   return (
     <div>
-      <Modal isOpen={isOpen} onRequestClose={() => closeModal} style={customStyles} ariaHideApp={false}>
-      <div style={{
-			backgroundColor:"white", 
-			borderRadius: "8px",
-			margin: "0",
-			width:"100%",
-			height: "100%",
-			display: "flex",
-			flexDirection: "column",
-			alignItems: "stretch",
-		}}>
-			<div style={{margin: "10px", textAlign:"left"}}>
-				<span style={{fontSize: "18px"}}>
-          <strong>알림</strong>
-        </span>
-        <span style={{fontSize: "18px", margin: "100px", textAlign:"right"}}>
-          <span style={{color: "#a20131", fontWeight: "700"}}>모의 수강 신청</span> 시스템
-        </span>
-			</div>
-			<hr style={{marginTop: "10px", width: "95%", height: "2px", backgroundColor: "rgba(0,0,0,0.05)", border: "0",}}/>
-			<div style={{
-				margin: "10px 10px",
-				paddingTop: "10px",
-				textAlign: "left",
-				fontSize: "16px",
-        fontWeight: "500",
-        height: "100px",
-        paddingLeft: "40px",
-			}}>
-				본 페이지는 <span style={{color: "#a20131", fontWeight: "800", lineHeight: "2"}}>모의</span> 수강신청 시스템입니다.<br/>
-        수강 신청 결과에 대한 어떠한 책임도 지지 않습니다.
-			</div>
-			<hr style={{marginTop: "10px", width: "95%", height: "2px", backgroundColor: "rgba(0,0,0,0.05)", border: "0",}}/>
-			<div style={{
-				margin: "auto"
-			}}>
-				<button onClick={() => closeModal()}
-					style={{
-					width: "50px",
-					height: "30px",
-					border:"0",
-					fontSize: "17px",
-					backgroundColor: "white",
-					color: "#a20131",
-          cursor: "pointer",
-				}}><strong>확인</strong>
-				</button>
-			</div>
-		</div>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => closeModal}
+        style={customStyles}
+        ariaHideApp={false}
+      >
+        <div
+          style={{
+            backgroundColor: "white",
+            borderRadius: "8px",
+            margin: "0",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "stretch",
+          }}
+        >
+          <div
+            style={{
+              margin: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                fontSize: "18px",
+                textAlign: "right",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+            >
+              <span style={{ color: "#a20131", fontWeight: "700" }}>
+                모의 수강 신청
+              </span>{" "}
+              시스템
+            </div>
+          </div>
+          <hr
+            style={{
+              marginTop: "10px",
+              width: "95%",
+              height: "2px",
+              backgroundColor: "rgba(0,0,0,0.05)",
+              border: "0",
+            }}
+          />
+          <div
+            style={{
+              margin: "10px 10px",
+              paddingTop: "10px",
+              textAlign: "left",
+              fontSize: "16px",
+              fontWeight: "500",
+              height: "100px",
+              paddingLeft: "40px",
+            }}
+          >
+            본 페이지는{" "}
+            <span
+              style={{ color: "#a20131", fontWeight: "800", lineHeight: "2" }}
+            >
+              모의
+            </span>{" "}
+            수강신청 시스템입니다.
+            <br />
+            수강 신청 결과에 대한 어떠한 책임도 지지 않습니다.
+          </div>
+          <hr
+            style={{
+              marginTop: "10px",
+              width: "95%",
+              height: "2px",
+              backgroundColor: "rgba(0,0,0,0.05)",
+              border: "0",
+            }}
+          />
+          <div
+            style={{
+              margin: "auto",
+            }}
+          >
+            <button
+              onClick={() => closeModal()}
+              style={{
+                width: "50px",
+                height: "30px",
+                border: "0",
+                fontSize: "17px",
+                backgroundColor: "white",
+                color: "#a20131",
+                cursor: "pointer",
+              }}
+            >
+              <strong>확인</strong>
+            </button>
+          </div>
+        </div>
       </Modal>
     </div>
   );
